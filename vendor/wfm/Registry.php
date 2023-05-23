452173809c7ff3c3019de525c29204702a0e175c
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-23 21:32:54 +0300

    created a static bounded array and created the setProperties method, where  specified the value of $value to a property named $name

--- vendor/wfm/Registry.php
+++ vendor/wfm/Registry.php
@@ -7,4 +7,13 @@ namespace wfm;
 class Registry
 {
     use TSingleton;
+
+    protected static array $properties = [];
+
+    public static function setProperties($name, $value)
+    {
+        self::$properties[$name] = $value;
+    }
+
 }
+
