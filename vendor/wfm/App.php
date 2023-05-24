2b826d6d560cce83533034ea1b95dbe11fb99836
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-24 11:15:29 +0300

    called the getParams method to fill in the settings

--- vendor/wfm/App.php
+++ vendor/wfm/App.php
@@ -11,15 +11,15 @@ class App
     public function __construct()
     {
         self::$app = Registry::getInstance();
+        $this->getParams();
     }
-
 
     public function getParams() #params for f-work
     {
         $params = require_once CONFIG . '/params.php';
         if (!empty($params)) {
             foreach ($params as $k => $v) {
-                self::$app->setProperties($k, $v);
+                self::$app->setProperties($k, $v); #name and values
             }
         }
     } #connect params fo f-work
