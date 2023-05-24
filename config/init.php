0331054e45fe19e4f97909f5f4e57dbdbda2fb16
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-24 14:23:51 +0300

    added config path to init

--- config/init.php
+++ config/init.php
@@ -7,6 +7,7 @@ define("APP", ROOT . '/app'); #the path to the application
 define("CORE", ROOT . '/vendor/wfm'); #wfm core
 define("CACHE", ROOT . '/temp/cache');
 define("LOGS", ROOT . '/temp/logs');
+define("CONFIG", ROOT . '/config');
 define("LAYOUT", 'ishop'); #default site template
 define("PATH", 'http://new-ishop.loc'); #the path to the main page of the site
 define("ADMIN", 'http://new-ishop.loc/admin'); #the path to the admin panel
