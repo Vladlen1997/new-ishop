3c789234d90bfb5779407f1a3aa86cacec0feee1
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-24 10:01:38 +0300

    created the returned parameters

---
+++ config/params.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'admin_email' => 'admin@new-ishop.loc',
+    'site_name' => 'I-Shop',
+    'pagination' => 3, #3 objects/1pages (кол-во элементов на отдельной странице, чтобы реализовать ту самую пагинацию)
+];
