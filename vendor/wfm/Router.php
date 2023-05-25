bb7e92fa965f4177900d7d810e187e5eb0d72b18
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-25 17:39:10 +0300

    In the route table, by the key that gets the first argument, we put what came in $route

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -8,4 +8,9 @@ class Router
     protected static array $routes = []; #routes table
     protected static array $route = [];
 
+    protected static function add($regexp, $route = [])
+    {
+        self::$routes[$regexp] = $route;
+    }
+
 }
