aec3a22ad00892baaf1eef8bfd08b1d555b72d4f
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-25 21:52:55 +0300

    added matchRoot method

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -8,7 +8,7 @@ class Router
     protected static array $routes = []; #routes table
     protected static array $route = [];
 
-    protected static function add($regexp, $route = [])
+    public static function add($regexp, $route = [])
     {
         self::$routes[$regexp] = $route;
     }
@@ -25,7 +25,16 @@ class Router
 
     public static function dispatch($url)
     {
-        var_dump($url);
+        if(self::matchRoot($url)) {
+            echo 'OK';
+        } else {
+            echo 'NO';
+        }
+    }
+
+    public static function matchRoot($url): bool #вызову func preg_match и сравню поступивший запрос с шаблоном регулярного выражения маршрутизатора
+    {
+        return false;
     }
 
 }
