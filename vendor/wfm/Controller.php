154c1778ae7de08edf85db7e03fb71f55a7f118f
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-26 19:01:03 +0300

    created getModel method, added check model object and created instance of the class

--- vendor/wfm/Controller.php
+++ vendor/wfm/Controller.php
@@ -7,9 +7,18 @@ abstract class Controller
     public array $meta = ['title' => '', 'description' => '', 'keywords' => '']; //из контроллера в шаблон передавать метаданные страницы - заголовки,  метоописание и ключевики страницы
     public false|string $layout = ''; #шаблон, в init определён (может быть как false, так и стринг)
     public string $views = ''; # только строка
-    public object $model; #также создаю объект модели
+    public object $model; #также создаю объект моделискуф
     public function __construct(public $route = []) #added construct and
     {
 
     }
+
+    public function getModel()
+    {
+        $model = 'app\models\\' . $this->route['admin_prefix'] . $this->route['controller'];
+        if(class_exists($model)) {  # создаём объект, только если есть соответствующий класс
+            $this->model = new $model();  # в итоге получаю модель, если таковая создана
+        }
+    }
+
 }
