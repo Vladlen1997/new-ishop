f7a93d03ec603592bdeef3141616bb9f2c2df39b
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-26 18:24:14 +0300

    created a minimum of properties that will be applied

--- vendor/wfm/Controller.php
+++ vendor/wfm/Controller.php
@@ -2,6 +2,12 @@
 
 abstract class Controller
 {
+
+    public array $data = []; #массив с данными
+    public array $meta = ['title' => '', 'description' => '', 'keywords' => '']; //из контроллера в шаблон передавать метаданные страницы - заголовки,  метоописание и ключевики страницы
+    public false|string $layout = ''; #шаблон, в init определён (может быть как false, так и стринг)
+    public string $views = ''; # только строка
+    public object $model; #также создаю объект модели
     public function __construct(public $route = []) #added construct and
     {
 
