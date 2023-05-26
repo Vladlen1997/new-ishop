11eb850dbd1861501210cf871e1e745ee405efe5
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-05-26 20:21:33 +0300

    I create an object of the view class, specify parameters and call from the render method

--- vendor/wfm/Controller.php
+++ vendor/wfm/Controller.php
@@ -8,6 +8,7 @@ abstract class Controller
     public false|string $layout = ''; #шаблон, в init определён (может быть как false, так и стринг)
     public string $views = ''; # только строка
     public object $model; #также создаю объект моделискуф
+
     public function __construct(public $route = []) #added construct and
     {
 
@@ -16,15 +17,17 @@ abstract class Controller
     public function getModel()
     {
         $model = 'app\models\\' . $this->route['admin_prefix'] . $this->route['controller'];
-        if(class_exists($model)) {  # создаём объект, только если есть соответствующий класс
+        if (class_exists($model)) {  # создаём объект, только если есть соответствующий класс
             $this->model = new $model();  # в итоге получаю модель, если таковая создана
         }
     }
+
     # создаю getView | view можно переопределять через контроллер
 
     public function getView()
     {
-        $this->views = $this->views ?: $this->route['action'];  #если $this->views не пустая строка, тогда запишем её, иначе название вида возьму по умолчанию
+        $this->views = $this->views ?: $this->route['action'];  # если $this->views не пустая строка, тогда запишем её, иначе название вида возьму по умолчанию
+        (new View($this->route, $this->layout, $this->views, $this->meta))->render($this->data); # создаю экземпляр класса view и пробрасываю параметры и от этого объекта вызываю метод render
     } # объект вида и метод рендер?
 
 
