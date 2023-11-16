623cac5efa209d67407abf53c2e28cbbed9cd13c
Vladlen Stankevich <vladlenstankevich1997@mail.ru>
2023-11-16 16:19:45 +0300

    Additionally, I create constants with file paths: helpers, cache and logs

--- configure/init.php
+++ configure/init.php
@@ -5,3 +5,6 @@ define("ROOT", dirname(__DIR__));
 define("WWW", ROOT . '/public');
 define("APP", ROOT . '/app');
 define("CORE", ROOT . '/vendor/appCore');
+define("HELPERS", ROOT . '/vendor/appCore/helpers');
+define("CACHE", ROOT . '/tmp/cache');
+define("LOGS", ROOT . '/tmp/logs');
